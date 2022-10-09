c5ee5a13132e782072af9030a68f1c68fc1ee741
Kuba_s <86146547+BroksDMG@users.noreply.github.com>
2022-10-09 17:45:01 +0200

    .

---
+++ complete-javascript-course-master/01-Fundamentals-Part-1/starter/assignments.js
@@ -0,0 +1,22 @@
+const country = "Poland";
+const continent = "Europ";
+let population = 38;
+console.log(country);
+console.log(continent);
+console.log(population);
+
+const isIsland = false;
+const language = 'polish';
+console.log(typeof isIsland);
+console.log(typeof population);
+console.log(typeof country);
+console.log(typeof language);
+const halfPopulation = population / 2;
+console.log(halfPopulation + 1);
+const populationOfFinlad = 8;
+console.log(population > populationOfFinlad);
+const averagePopulation = 33;
+console.log(population > averagePopulation);
+const description = 'Portugal is in Europe ,and its 11 milion people speak portuguese';
+console.log(description);
+
---
+++ complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,53 @@
+// let js = 'amazing';
+// console.log(40 + 9 + 23 - 10);
+
+// let firstName = 'Jonas';
+// console.log(firstName);
+//////////////////////////////////////////////////////////////////////////
+// math operators
+// const now = 2037;
+// const ageJonas = now - 1991;
+// const ageSarah = now - 2018;
+
+// console.log(ageJonas, ageSarah);
+
+// console.log(ageJonas * 2, ageJonas / 10, 2 ** 3);
+// // 2**3 means 2to the power of 3 = 2* 2*2
+
+// const firstName = 'Jonas';
+// const lastName = 'schedtmann';
+// console.log(firstName + ' ' + lastName);
+// //assigment operators
+// let x = 10 + 5;    //15
+// x += 10;  //x =x+10=25
+// x *= 4; // x =x*4 =100
+// x++; //x=x+1
+// x--;
+// x--;
+
+// console.log(x);
+// // Copatison operators
+// console.log(ageJonas > ageSarah); // >,< , >=,<=
+// console.log(ageSarah >= 18);
+
+// const isFillAge = ageSarah >= 18;
+
+// console.log(now - 1991 > now - 2018);
+
+////////////////////////////////////////////////////////////////////////////////////
+
+const now = 2037;
+const ageJonas = now - 1991;
+const ageSarah = now - 2018;
+
+console.log(now - 1991 > now - 2018);
+
+console.log(25 - 10 - 5);
+
+console.log(25 + 10 - 5 ** 2);
+console.log(5 ** 2);
+let x, y;
+x = y = 25 - 10 - 5; // x= y =10, x =10
+console.log(x, y);
+const averageAge = (afeJonas + ageSarag) / 2
+console.loh(ageJonas, ageSarah)
