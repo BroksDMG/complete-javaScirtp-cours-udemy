c71a4a77695ed169114fd8cd44876783f3185646
Kuba_s <86146547+BroksDMG@users.noreply.github.com>
2022-10-13 19:43:25 +0200

    .

--- complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
+++ complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
@@ -113,40 +113,48 @@
 // const friend2 = 'Steven';
 // const friend3 = 'Peter';
 
+// const friends = ['Michael', 'Steven', 'Peter'];
+// console.log(friends);
+
+// const y = new Array(1991, 1984, 2008, 2020);
+
+// console.log(friends[0]);
+// console.log(friends[2]);
+
+// console.log(friends.length);
+// console.log(friends[friends.length - 1]);
+
+// friends[2] = 'Jay';
+// console.log(friends);
+// // friens= ['Bob',"Alice"];
+// const firstName = 'Jonas';
+// const jonas = [firstName, 'Schmedtmann', 2037 - 1991,
+//     'teacher', friends];
+
+// console.log(jonas);
+// console.log(jonas.length);
+
+// ///Exercise
+// function calcAge(brithYeah) {
+//     return 2037 - brithYeah;
+// }
+// const years = [1990, 1967, 2002, 2010, 2018];
+
+// const age1 = calcAge(years[0]);
+// const age2 = calcAge(years[1]);
+// const age3 = calcAge(years[years.length - 1]);
+// console.log(age1, age2, age3);
+
+// const ages = [calcAge(years[0]), calcAge(years[1]),
+// calcAge(years[1])];
+// console.log(ages)
+
+
+/////////////////////////////////////////////////
+
+
 const friends = ['Michael', 'Steven', 'Peter'];
+
+const newLength = friends.push('Jay');
 console.log(friends);
-
-const y = new Array(1991, 1984, 2008, 2020);
-
-console.log(friends[0]);
-console.log(friends[2]);
-
-console.log(friends.length);
-console.log(friends[friends.length - 1]);
-
-friends[2] = 'Jay';
-console.log(friends);
-// friens= ['Bob',"Alice"];
-const firstName = 'Jonas';
-const jonas = [firstName, 'Schmedtmann', 2037 - 1991,
-    'teacher', friends];
-
-console.log(jonas);
-console.log(jonas.length);
-
-///Exercise
-function calcAge(brithYeah) {
-    return 2037 - brithYeah;
-}
-const years = [1990, 1967, 2002, 2010, 2018];
-
-const age1 = calcAge(years[0]);
-const age2 = calcAge(years[1]);
-const age3 = calcAge(years[years.length - 1]);
-console.log(age1, age2, age3);
-
-const ages = [calcAge(years[0]), calcAge(years[1]),
-calcAge(years[1])];
-console.log(ages)
-
-
+console.log(newLength);
