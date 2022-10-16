347d3ee0967010200992f86a9ffb08cb6241f885
Kuba_s <86146547+BroksDMG@users.noreply.github.com>
2022-10-16 23:48:07 +0200

    .

--- complete-javascript-course-master/05-Guess-My-Number/starter/script.js
+++ complete-javascript-course-master/05-Guess-My-Number/starter/script.js
@@ -1,9 +1,10 @@
 'use strict';
 
-console.log(document.querySelector('.message').textContent);
-document.querySelector('.number').textContent = 13;
-document.querySelector('.score').textContent = 10;
+// console.log(document.querySelector('.message').textContent);
+// document.querySelector('.number').textContent = 13;
+// document.querySelector('.score').textContent = 10;
 
-document.querySelector('.guess').value =23;
+// document.querySelector('.guess').value =23;
 
-console.log(document.querySelector('.guess').value);
+// console.log(document.querySelector('.guess').value);
+
