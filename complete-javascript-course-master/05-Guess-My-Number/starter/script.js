3615217fd2e58b2b20e20d1aa35710224984bab4
Kuba_s <86146547+BroksDMG@users.noreply.github.com>
2022-10-17 13:19:21 +0200

    Gusss number game

--- complete-javascript-course-master/05-Guess-My-Number/starter/script.js
+++ complete-javascript-course-master/05-Guess-My-Number/starter/script.js
@@ -7,4 +7,49 @@
 // document.querySelector('.guess').value =23;
 
 // console.log(document.querySelector('.guess').value);
+ const docSel =function (argument) {
+    return document.querySelector(argument)
+ }
+const secretNumber = Math.floor(Math.random()*20)+1;
+docSel('.number').textContent= secretNumber;
 
+//chech guess number is number?
+//if is lower show "numebr is higher" and socre -1
+//if is higher show "numebr is lover" and score -1
+// if is correct show "congratulation" and save highscore
+// lose the game if score is lower than 1
+// fix score string
+
+let score =20;
+//
+docSel('.check').addEventListener('click',function (){
+    
+    const guessNumebr = Number(docSel('.guess').value);
+    console.log(guessNumebr,typeof guessNumebr)
+    if(!guessNumebr){
+        docSel('.message').textContent = " it's not a number";
+        score--;
+        docSel('.label-score').textContent=score;
+    }else if(guessNumebr> secretNumber){
+        docSel('.message').textContent = "📈 To high!"
+        score--;
+        docSel('.label-score').textContent=score;
+    }
+    else if(guessNumebr< secretNumber){
+        docSel('.message').textContent = "📉 To low!"
+        score--;
+        docSel('.label-score').textContent=score;
+    }
+    else if(guessNumebr === secretNumber){
+        docSel('.message').textContent = "🎉 Congratulation!"
+
+    }
+    else if(score<1){
+        docSel('.message').textContent = "😿You lose the game😿"
+        
+    }
+
+    
+
+
+})
