92f7d448898b663b1949e00eee2fa23032c852a7
Kuba_s <86146547+BroksDMG@users.noreply.github.com>
2022-10-22 18:29:59 +0200

    .

--- complete-javascript-course-master/06-Modal/starter/script.js
+++ complete-javascript-course-master/06-Modal/starter/script.js
@@ -1,1 +1,12 @@
 'use strict';
+
+const showModal =document.querySelectorAll('.show-modal');
+const hiddenModal = document.querySelector('.hiden');
+const closeModal = document.querySelector('.close-modal');
+const hiddenOverlay = document.querySelector('.overlay');
+
+showModal.addEventListener('click', function(){
+    for(let i = 0; i<showModal;i++)
+    console.log(showModal);
+
+})
