23b13e3d32c3d53a03284da84449131cbdd9c8a1
Kuba_s <86146547+BroksDMG@users.noreply.github.com>
2022-10-27 14:50:56 +0200

    pig game start

--- complete-javascript-course-master/07-Pig-Game/starter/script.js
+++ complete-javascript-course-master/07-Pig-Game/starter/script.js
@@ -1,1 +1,11 @@
 'use strict';
+
+//select elements
+const scorPL0= document.getElementById('score--0');
+const scorPL1= document.getElementById('score--1');
+const dice = document.querySelector('.dice');
+//start conditions
+scorPL0.textContent=0;
+scorPL1.textContent=0;
+dice.classList.add('hidden');
+
