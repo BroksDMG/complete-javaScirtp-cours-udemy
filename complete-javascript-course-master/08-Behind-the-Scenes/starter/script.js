e690354824d4693f2d895d0006e323232c9def6a
Kuba_s <86146547+BroksDMG@users.noreply.github.com>
2022-10-29 20:05:42 +0200

    Primitvies and object

--- complete-javascript-course-master/08-Behind-the-Scenes/starter/script.js
+++ complete-javascript-course-master/08-Behind-the-Scenes/starter/script.js
@@ -91,33 +91,85 @@
  ///////////////////////this///////////////////////
  
 
-//  console.log(this);
+// //  console.log(this);
  
- const calcAge = function (birthYear) {
-    // console.log(2037-birthYear);
-    // console.log(this);                  //display undifanded
- };
-calcAge(1991);
+//  const calcAge = function (birthYear) {
+//     // console.log(2037-birthYear);
+//     // console.log(this);                  //display undifanded
+//  };
+// calcAge(1991);
 
-const calcAgeArrow = birthYear=>{
-    // console.log(2037-birthYear);
-    // console.log(this);                  //display value object window
+// const calcAgeArrow = birthYear=>{
+//     // console.log(2037-birthYear);
+//     // console.log(this);                  //display value object window
+// };
+// calcAgeArrow(1991);
+
+
+// const jonas = {
+//     year:1991,
+//     calcAge:function(){
+
+//         console.log(this);
+//         console.log(2037-this.year);
+//     }
+// }
+// jonas.calcAge();
+
+// const matilda={
+//     year:2017,
+// };
+// matilda.calcAge=jonas.calcAge;
+// matilda.calcAge();
+
+//////////////////regular functions vs arrow functions////////////////
+
+// let age= 30;
+// let oldAge= age;
+// age =31;
+// console.log(age);
+// console.log(oldAge);
+
+// const me ={
+//     name:'Jonas',
+//     age:30,
+// }
+// const friend =me;
+// friend.age=26;
+// console.log('freind:',friend);
+// console.log('Me',me);
+///////////////////////primitive values and reference values/////////
+//primitive tyoes
+let lasName= "williams";
+let oldLastName=lasName;
+lasName='davis';
+console.log(lasName,oldLastName);
+//reference type
+const jessica={
+    firstName:'Jessica',
+    lasName:'Williams',
+    age:27,
 };
-calcAgeArrow(1991);
+const marriedJessica = jessica;
+marriedJessica.lasName='Davids';
+console.log('Before marriage:',jessica);
+console.log('After marrage:',marriedJessica);
 
-
-const jonas = {
-    year:1991,
-    calcAge:function(){
-
-        console.log(this);
-        console.log(2037-this.year);
-    }
-}
-jonas.calcAge();
-
-const matilda={
-    year:2017,
+//Copying obejcts
+const jessica2={
+    firstName:'Jessica',
+    lasName:'Williams',
+    age:27,
+    familly:['Alice',"Bob"]
 };
-matilda.calcAge=jonas.calcAge;
-matilda.calcAge();
+
+const jessicaCopy= Object.assign({},jessica2);
+
+jessicaCopy.lasName='Davis';
+console.log('Before marriage:',jessica2);
+console.log('After marrage:',jessicaCopy);
+
+jessicaCopy.familly.push('Mary');
+jessicaCopy.familly.push('john');
+console.log('Before marriage:',jessica2);
+console.log('After marrage:',jessicaCopy);
