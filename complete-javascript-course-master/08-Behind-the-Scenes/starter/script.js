85a6de7d4a195d4851c1810cb39c17036f0e5f25
Kuba_s <86146547+BroksDMG@users.noreply.github.com>
2022-10-29 14:53:12 +0200

    bts part 1-scope in practic

--- complete-javascript-course-master/08-Behind-the-Scenes/starter/script.js
+++ complete-javascript-course-master/08-Behind-the-Scenes/starter/script.js
@@ -1,1 +1,29 @@
 'use strict';
+
+function clacAge(birthYear){
+    const age = 2037-birthYear;
+
+    function printAge(){
+
+        let output =`${firstName},you are ${age},born in ${birthYear}`
+        
+        console.log(output);
+        if(birthYear>=1981&&birthYear<=1996){
+            var millenial =true;
+            const firstName="Steven";
+            const str=`oh, you're a millenial, ${firstName}`;
+            console.log(str);
+        
+            function add (a,b){
+                return a+b;
+            }
+            const output="NEW OUTPOT"
+        }
+        console.log(millenial);
+        console.log(output);
+    }
+    printAge()
+    return age;
+}
+const firstName = 'jonas';
+clacAge(1991);
