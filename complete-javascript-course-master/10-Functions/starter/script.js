7ba630bbde09397220366746828a461b767792b3
Kuba_s <86146547+BroksDMG@users.noreply.github.com>
2023-01-28 22:03:32 +0100

    immediately invoked function Expresion

--- complete-javascript-course-master/10-Functions/starter/script.js
+++ complete-javascript-course-master/10-Functions/starter/script.js
@@ -91,86 +91,96 @@
 
 /////////////// the call and apply methods////////
 
-const lufthsnsa = {
-    airline:'Luufthansa',
-    iataCode:'LH',
-    bookings:[],
-    book(flightNum,name){
-        console.log(`${name} booked a seat on ${this.airline}
-        flight ${this.iataCode}${flightNum}`
-    )
-    this.bookings.push({flight:
-        `${this.iataCode}${flightNum}`,name})
-},
-};
+// const lufthsnsa = {
+//     airline:'Luufthansa',
+//     iataCode:'LH',
+//     bookings:[],
+//     book(flightNum,name){
+//         console.log(`${name} booked a seat on ${this.airline}
+//         flight ${this.iataCode}${flightNum}`
+//     )
+//     this.bookings.push({flight:
+//         `${this.iataCode}${flightNum}`,name})
+// },
+// };
 
-lufthsnsa.book(239,'Jonas Schmedtman');
-lufthsnsa.book(635,'John Smith')
+// lufthsnsa.book(239,'Jonas Schmedtman');
+// lufthsnsa.book(635,'John Smith')
 
-const eurowings = {
-    name:'Eurowings',
-    iataCode:'Ew',
-    bookings:[],
-};
+// const eurowings = {
+//     name:'Eurowings',
+//     iataCode:'Ew',
+//     bookings:[],
+// };
 
-const book = lufthsnsa.book;
-///does not working///
-// book(23,'Sarah Williams')
-book.call(eurowings,23,"Sarah Williams");
-// console.log(eurowings);
+// const book = lufthsnsa.book;
+// ///does not working///
+// // book(23,'Sarah Williams')
+// book.call(eurowings,23,"Sarah Williams");
+// // console.log(eurowings);
 
-book.call(lufthsnsa,239,'Mary Coper')
-// console.log(lufthsnsa);
+// book.call(lufthsnsa,239,'Mary Coper')
+// // console.log(lufthsnsa);
 
-const swiss={
-    airline:'Swiss Air Lines',
-    iataCode:'LX',
-    bookings: [],
-};
-//apply method
-//befor ES5
-book.apply(swiss,[528,'George Cooper'])
-//after ES5
-book.call(swiss,...[314,"Megan Cooper"])
-book.call(swiss,[314,"Adam Cooper"])
-// console.log(swiss);
+// const swiss={
+//     airline:'Swiss Air Lines',
+//     iataCode:'LX',
+//     bookings: [],
+// };
+// //apply method
+// //befor ES5
+// book.apply(swiss,[528,'George Cooper'])
+// //after ES5
+// book.call(swiss,...[314,"Megan Cooper"])
+// book.call(swiss,[314,"Adam Cooper"])
+// // console.log(swiss);
 
-//////////////// bind method ///////////////
-const bookEW= book.bind(eurowings);
-const bookLH= book.bind(lufthsnsa);
-const bookLX= book.bind(swiss);
+// //////////////// bind method ///////////////
+// const bookEW= book.bind(eurowings);
+// const bookLH= book.bind(lufthsnsa);
+// const bookLX= book.bind(swiss);
 
-bookEW(23,'Steven Williams');
-const bookEw23= book.bind(eurowings, 23);
-bookEw23('Jonas Schmedtmann');
-bookEw23('Marta Cooper');
-/////////////// with Event Listeners///
+// bookEW(23,'Steven Williams');
+// const bookEw23= book.bind(eurowings, 23);
+// bookEw23('Jonas Schmedtmann');
+// bookEw23('Marta Cooper');
+// /////////////// with Event Listeners///
 
-lufthsnsa.planes = 300;
-lufthsnsa.buyPlane=function(){
+// lufthsnsa.planes = 300;
+// lufthsnsa.buyPlane=function(){
     
-    console.log(this);
-    this.planes++;
-    console.log(this.planes);
+//     console.log(this);
+//     this.planes++;
+//     console.log(this.planes);
+// }
+// // const test=lufthsnsa.buyPlane()
+
+
+// document
+// .querySelector('.buy')
+// .addEventListener('click',lufthsnsa.buyPlane.bind(lufthsnsa))
+
+// // partial application
+// const addTax =(rate,value)=>value+value*rate;
+// console.log(addTax(0.1,200));
+
+// const addVAT=addTax.bind(null,0.23);
+
+// console.log(addVAT(100));
+// console.log(addVAT(23));
+// ///practis with returning function//
+
+// const setVAT=(rate)=>(value)=>value+value*rate;
+// const addTaxWithVAT= setVAT(0.23);
+// console.log(addTaxWithVAT(100));
+// console.log(addTaxWithVAT(23));
+
+const runOnce =function(){
+    console.log("this will never run again");
 }
-// const test=lufthsnsa.buyPlane()
+runOnce();
 
-
-document
-.querySelector('.buy')
-.addEventListener('click',lufthsnsa.buyPlane.bind(lufthsnsa))
-
-// partial application
-const addTax =(rate,value)=>value+value*rate;
-console.log(addTax(0.1,200));
-
-const addVAT=addTax.bind(null,0.23);
-
-console.log(addVAT(100));
-console.log(addVAT(23));
-///practis with returning function//
-
-const setVAT=(rate)=>(value)=>value+value*rate;
-const addTaxWithVAT= setVAT(0.23);
-console.log(addTaxWithVAT(100));
-console.log(addTaxWithVAT(23));
+//II
+(function(){
+    console.log('this will never run again');
+})();
