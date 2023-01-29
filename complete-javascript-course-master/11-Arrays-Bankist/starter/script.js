b9d95d5d1321374e2fdb2b82b32f7ef7cc0fb9c9
Kuba_s <86146547+BroksDMG@users.noreply.github.com>
2023-01-29 21:38:03 +0100

    method at

--- complete-javascript-course-master/11-Arrays-Bankist/starter/script.js
+++ complete-javascript-course-master/11-Arrays-Bankist/starter/script.js
@@ -75,32 +75,46 @@ const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 /////////////////////////////////////////////////
 
-let arr =['a','b','c','d','e'];
+// let arr =['a','b','c','d','e'];
 
-//slice
-console.log(arr.slice(2));
-console.log(arr.slice(2,4));
-console.log(arr.slice(-2));
-console.log(arr.slice(-1));
-console.log(arr.slice(1,-2));
-console.log(arr.slice());
-console.log([...arr]);
+// //slice
+// console.log(arr.slice(2));
+// console.log(arr.slice(2,4));
+// console.log(arr.slice(-2));
+// console.log(arr.slice(-1));
+// console.log(arr.slice(1,-2));
+// console.log(arr.slice());
+// console.log([...arr]);
 
-// splice
-arr.splice(-3);
-console.log(arr);
-arr.splice(1,2);
-console.log(arr);
+// // splice
+// arr.splice(-3);
+// console.log(arr);
+// arr.splice(1,2);
+// console.log(arr);
 
-//reverse'
-arr =['a','b','c','d','e'];
-const arr2=['j','i','h','g','f'];
-console.log(arr2.reverse());
-console.log(arr2);
+// //reverse'
+// arr =['a','b','c','d','e'];
+// const arr2=['j','i','h','g','f'];
+// console.log(arr2.reverse());
+// console.log(arr2);
 
-//concat 
-const letters = arr.concat(arr2);
-console.log(letters);
-console.log(...arr,...arr2);
-//join
-console.log(letters.join(''));
+// //concat 
+// const letters = arr.concat(arr2);
+// console.log(letters);
+// console.log(...arr,...arr2);
+// //join
+// console.log(letters.join(' - '));
+
+/// new method at
+
+const arr =[23,11,64];
+console.log(arr[0]);
+console.log(arr.at(0));
+/////////geting last array element
+
+console.log(arr[arr.length-1]);
+console.log(arr.slice(-1)[0]);
+console.log(arr.at(-1));
+// with strings
+console.log('jonas'.at(0));
+console.log('jonas'.at(-1));
