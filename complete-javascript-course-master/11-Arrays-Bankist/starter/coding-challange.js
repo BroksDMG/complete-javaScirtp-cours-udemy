e6d9a82bc31f5f7c9bede31f4206a136285caf1f
Kuba_s <86146547+BroksDMG@users.noreply.github.com>
2023-02-03 12:53:47 +0100

    coding challange #3

--- complete-javascript-course-master/11-Arrays-Bankist/starter/coding-challange.js
+++ complete-javascript-course-master/11-Arrays-Bankist/starter/coding-challange.js
@@ -18,12 +18,23 @@ function checkDogs (dogsJulia,dogsKate){
 console.log('///////////////////ANOTHER DATA//////////////////');
 // checkDogs(juliData2,kateData2);
 
-function calcAveratgeHumanAge (dogAges){
-    const calc =dogAges.map(age=>age<=2?2*age:16+age*4)
-    const leesThan18 =calc.filter(age=>age>=18)
-    const averages =leesThan18.reduce((acc,age)=>acc+age/leesThan18.length,0)
-    console.log(calc);
-    console.log(leesThan18);
+// function calcAveratgeHumanAge (dogAges){
+//     const calc =dogAges.map(age=>age<=2?2*age:16+age*4)
+//     const leesThan18 =calc.filter(age=>age>=18)
+//     const averages =leesThan18.reduce((acc,age)=>acc+age/leesThan18.length,0)
+//     console.log(calc);
+//     console.log(leesThan18);
+//     return averages
+// }
+// console.log(calcAveratgeHumanAge([5, 2, 4, 1, 15, 8, 3]));
+// console.log(calcAveratgeHumanAge([16, 6, 10, 5, 6, 1, 4]));
+// ///rewrite function
+
+const calcAveratgeHumanAge=(dogAge)=>{
+    const averages=dogAge
+        .map(age=>age<=2?2*age:16+age*4)
+        .filter(age=>age>=18)
+        .reduce((acc,age,i,arr)=>acc+age/arr.length,0)
     return averages
 }
 console.log(calcAveratgeHumanAge([5, 2, 4, 1, 15, 8, 3]));
--- complete-javascript-course-master/11-Arrays-Bankist/starter/script.js
+++ complete-javascript-course-master/11-Arrays-Bankist/starter/script.js
@@ -69,7 +69,7 @@ const displayMovments = function(movements){
   `
   <div class="movements__row">
     <div class="movements__type movements__type--${type}">${i+1} ${type}</div>
-    <div class="movements__value">${mov}</div>
+    <div class="movements__value">${mov}€</div>
   </div>
   `
   containerMovements.insertAdjacentHTML("afterbegin",html);
@@ -79,10 +79,31 @@ displayMovments(account1.movements)
 
 const calcDisplayBalance = function(movement){
   const balance = movement.reduce((acc,mov)=>acc+mov,0);
-  labelBalance.textContent=`${balance} EUR`;
+  labelBalance.textContent=`${balance} €`;
 }
 calcDisplayBalance(account1.movements);
 ///inicjały z obiektów
+const calcDisplaySummary = function(movements){
+  const incomes= movements
+    .filter(mov=>mov>0)
+    .reduce((acc,mov)=>acc+mov,0);
+    labelSumIn.textContent=`${incomes}€`
+  const outcoms =movements
+    .filter(mov=>mov<0)
+    .reduce((acc,mov)=>acc+mov,0);
+    labelSumOut.textContent=`${Math.abs(outcoms)}€`
+
+  const iterest =movements.filter(mov=>mov>0)
+    .map(deposit=>(deposit*1.2)/100)
+    .filter((int,i,arr)=>{
+      console.log(arr);
+      return int>=i;
+    })
+    .reduce((acc,int)=>acc+int,0);
+    labelSumInterest.textContent=`${iterest}€` 
+}
+calcDisplaySummary(account1.movements)
+
 function createUsernames (accs){
   accs.forEach(acc=>{
     acc.username=acc.owner
